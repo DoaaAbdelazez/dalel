dc50abf9bdc28aca10bea3d09d185590503dd037
Doaa Abdelazez <abdelazezdoaa30@gmail.com>
2024-01-26 17:17:36 +0200

    App Colors

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,32 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesForgetPassword
+  /// assets/images/forgetPassword.png
+  static const String assetsImagesForgetPassword = "assets/images/forgetPassword.png";
+
+  /// Assets for assetsImagesOn1
+  /// assets/images/on1.png
+  static const String assetsImagesOn1 = "assets/images/on1.png";
+
+  /// Assets for assetsImagesOn2
+  /// assets/images/on2.png
+  static const String assetsImagesOn2 = "assets/images/on2.png";
+
+  /// Assets for assetsImagesOn3
+  /// assets/images/on3.png
+  static const String assetsImagesOn3 = "assets/images/on3.png";
+
+  /// Assets for assetsImagesSuccces1
+  /// assets/images/Succces1.png
+  static const String assetsImagesSuccces1 = "assets/images/Succces1.png";
+
+  /// Assets for assetsImagesVector1
+  /// assets/images/Vector1.svg
+  static const String assetsImagesVector1 = "assets/images/Vector1.svg";
+
+  /// Assets for assetsImagesVector2
+  /// assets/images/Vector2.svg
+  static const String assetsImagesVector2 = "assets/images/Vector2.svg";
+}
+
